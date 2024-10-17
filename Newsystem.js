b9f9329d1da0055b3dffd7849ff8f4c90a037fb5
Botmines00 <ramos061ofc@gmail.com>
2024-10-17 16:14:44 -0300

    Update Newsystem.js

--- Newsystem.js
+++ Newsystem.js
@@ -14,19 +14,31 @@ javascript:(function() {
     function createMenu() {
         const m = document.createElement('div');
         Object.assign(m.style, {
-            position: 'fixed', top: '30%', left: '30%', width: '350px',
-            background: '#1e1e1e', color: '#fff', padding: '10px', borderRadius: '8px', 
-            border: '2px solid #fe5f2f', boxShadow: '0 0 10px rgba(0,0,0,0.5)', display: 'none', zIndex: '9999',
-            display: 'flex', alignItems: 'center', minHeight: '100px', maxHeight: '200px', overflow: 'hidden'
+            position: 'fixed',
+            top: '30%',
+            left: '30%',
+            width: '350px',
+            background: '#1e1e1e',
+            color: '#fff',
+            padding: '10px',
+            borderRadius: '8px',
+            border: '2px solid #fe5f2f',
+            boxShadow: '0 0 10px rgba(0,0,0,0.5)',
+            display: 'none',
+            zIndex: '9999',
+            display: 'flex',
+            alignItems: 'center',
+            minHeight: '100px',
+            overflow: 'hidden'
         });
 
         m.innerHTML = `
             <img src="https://i.ibb.co/6r4yRDT/Proxy-sha-256-20241010-125036-0000.jpg" style="width: 80px; height: 80px; border-radius: 50%; border: 2px solid #fe5f2f; margin-right: 15px;">
-            <div style="flex: 1; display: flex; flex-direction: column;">
+            <div style="flex: 1; display: flex; justify-content: space-between; align-items: center;">
                 <h3 style='margin: 0; padding: 0; font-size: 18px;'>New System 00 <i class="fas fa-check-circle" style="color: #fe5f2f;"></i></h3>
-                <span id='closeMenu' style="cursor: pointer; font-size: 24px; color: white; align-self: flex-end;">X</span>
+                <span id='closeMenu' style="cursor: pointer; font-size: 24px; color: white;">X</span>
             </div>
-            <div style='margin-left: 10px; font-size: 14px;'>
+            <div style='margin-left: 10px; font-size: 14px; display: flex; align-items: center;'>
                 <i class="fas fa-cogs"></i> SHA256 | <i class="fas fa-info-circle"></i> Versão: 4.0
             </div>
         `;
