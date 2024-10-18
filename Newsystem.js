6f973e7d896e27ed78a8b3b63fb4114246ebdb32
Botmines00 <ramos061ofc@gmail.com>
2024-10-18 11:57:27 -0300

    Update Newsystem.js

--- Newsystem.js
+++ Newsystem.js
@@ -46,14 +46,16 @@ javascript:(function() {
             </div>
             <div style="margin-top: 10px; text-align: center;">
                 <div style="display: flex; align-items: center; gap: 5px;">
-                    <span class="chance" style="color: #00FF00; font-weight: bold;">Chance: 99.99%</span>
+                    <span class="chance" style="color: #2ecc71; font-weight: bold;">Chance: 99.99%</span>
                 </div>
                 <div style="display: flex; align-items: center; gap: 5px;">
                     Entrar no: <span class="colorIndicator">🔴</span>
                 </div>
-                <div id="winMessage" style="color: #00FF00; font-weight: bold; display: none;"></div>
+                <div id="winMessage" style="color: green; font-weight: bold; display: none;"></div>
                 <div style="margin-top: 10px; font-size: 12px; color: #00FF00;">
-                    SHA256 | Versão: 4.0
+                    <div style="background-color: rgba(255, 255, 255, 0.1); padding: 5px; border-radius: 5px;">
+                        SHA256 | Versão: 4.0
+                    </div>
                 </div>
             </div>
         `;
